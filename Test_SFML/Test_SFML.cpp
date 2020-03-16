7ef25087bccdf2aba96e06de2c924f35551f0e8d
Phuong <phuong.tran@jvncomm.com>
2020-03-16 12:56:13 -0400

    It's moving

--- Test_SFML/Test_SFML.cpp
+++ Test_SFML/Test_SFML.cpp
@@ -1,7 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
+constexpr float PI = 3.14159265f;
+
 int main()
 {
+	float ballSpeed = 0.2f;
+	float ballAngleRad = 90 * (PI / 180);
+	sf::Clock clock;
+
 	sf::RenderWindow window(sf::VideoMode(520, 520), "Hello World!");
 
 	sf::RectangleShape frame(sf::Vector2f(500, 500));
@@ -9,6 +16,12 @@ int main()
 	frame.setOutlineThickness(5.0f);
 	frame.setFillColor(sf::Color(0,0,0,0));
 	frame.setPosition(10, 10);
+
+	sf::CircleShape ball(50.0f);
+	ball.setFillColor(sf::Color::Green);
+	ball.setPosition(100, 100);
+
+	auto frame_bound = frame.getGlobalBounds();
 
 	while (window.isOpen())
 	{
@@ -22,7 +35,24 @@ int main()
 
 		window.clear();
 
+		if (ball.getPosition().y + 50 > 500)
+		{
+			ballAngleRad = -ballAngleRad;
+		}
+
+		if (ball.getPosition().y - 50 < 0)
+		{
+			ballAngleRad = -ballAngleRad;
+		}
+
+		double elapsed = clock.restart().asMilliseconds();
+		auto new_x = elapsed * ballSpeed * std::cos(ballAngleRad);
+		auto new_y = elapsed * ballSpeed * std::sin(ballAngleRad);
+		std::cout << new_x << ", " << new_y << "\n";
+		ball.move(new_x, new_y);
+
 		window.draw(frame);
+		window.draw(ball);
 
 		window.display();
 	}
